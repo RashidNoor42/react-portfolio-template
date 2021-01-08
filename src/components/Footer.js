a76fd4925565c8cc594322c3a454c05d73f3edd5
RashidNoor42 <49569582+RashidNoor42@users.noreply.github.com>
2021-01-08 10:51:08 +0500

    Update Footer.js
    new footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,7 +27,7 @@ const Footer = () => {
           </div>
           <span>
             Made With <icon>❤</icon> by{" "}
-            <a href="https://www.chetanverma.com/">Chetan Verma</a>
+            <a href="https://www.rashidnoor.me/">Rashid Noor</a>
           </span>
         </div>
       </div>
