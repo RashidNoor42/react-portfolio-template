7e91b41d9dda9367240845ae795db3b334afd038
RashidNoor42 <49569582+RashidNoor42@users.noreply.github.com>
2021-01-10 20:06:42 +0500

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -12,7 +12,7 @@ const About = () => {
               <h1>About Me</h1>
             </Fade>
             <p>
-     I've worked for multiple years and highly
+              I've worked for multiple years and highly
               qualified in Augmented Reality, Web & Mobile
               application Development to create impressive and
               interactive solutions while transforming ideas into
@@ -21,24 +21,25 @@ const About = () => {
               stimulate the process of learning and growing in
               professional field at the same time with full satisfaction.
               Eager to learn more software tools and methodologies
-              for the continuity of utilizing knowledge in best ways.
-              My Abilities are:
-              - AR, Laravel and MERN Developer
-              - Laravel Framework Trainer
-              - Teaching Assistant
-              - Leadership
-              - MentoringJava Language
-              Languages
-              English
-              Urdu
-              Punjabi
-              - Collaboration and Team Management
-              - Negotiation and Conflict Management
-              - Public Speaker
-              - Analytical Expert
-              - Volunteer
-              - Traveler
-              - Writer
+              for the continuity of utilizing knowledge in best ways.<br>
+             <p> My Abilities are:<br>
+              - AR, Laravel and MERN Developer<br>
+              - Laravel Framework Trainer<br>
+              - Teaching Assistant<br>
+              - Leadership<br>
+              - Mentoring<br>
+              - Java Language</p>
+              Languages<br>
+              English<br>
+              Urdu<br>
+              Punjabi<br>
+              - Collaboration and Team Management<br>
+              - Negotiation and Conflict Management<br>
+              - Public Speaker<br>
+              - Analytical Expert<br>
+              - Volunteer<br>
+              - Traveler<br>
+              - Writer<br>
             </p>
           </div>
           <div className="image-wrapper">
