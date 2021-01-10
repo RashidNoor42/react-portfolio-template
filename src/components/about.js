d4bf540db654a9797631cacb02c6a2d815ba87a1
RashidNoor42 <49569582+RashidNoor42@users.noreply.github.com>
2021-01-10 20:03:44 +0500

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -12,13 +12,33 @@ const About = () => {
               <h1>About Me</h1>
             </Fade>
             <p>
-              {data.aboutParaOne}
-              <br></br>
-              <br></br>
-              {data.aboutParaTwo}
-              <br></br>
-              <br></br>
-              {data.aboutParaThree}
+     I've worked for multiple years and highly
+              qualified in Augmented Reality, Web & Mobile
+              application Development to create impressive and
+              interactive solutions while transforming ideas into
+              realities, primarily focused on outcomes based learning
+              experiences, I'm engaging in industrial projects to
+              stimulate the process of learning and growing in
+              professional field at the same time with full satisfaction.
+              Eager to learn more software tools and methodologies
+              for the continuity of utilizing knowledge in best ways.
+              My Abilities are:
+              - AR, Laravel and MERN Developer
+              - Laravel Framework Trainer
+              - Teaching Assistant
+              - Leadership
+              - MentoringJava Language
+              Languages
+              English
+              Urdu
+              Punjabi
+              - Collaboration and Team Management
+              - Negotiation and Conflict Management
+              - Public Speaker
+              - Analytical Expert
+              - Volunteer
+              - Traveler
+              - Writer
             </p>
           </div>
           <div className="image-wrapper">
